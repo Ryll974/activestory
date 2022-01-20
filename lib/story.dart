be25a7804878c61f5686440e63eb2b3510444724
Ryll974 <jullien.patrick@gmail.com>
2022-01-20 12:48:29 +0400

    commit 3 -- modification de la classe Story dans story.dart pour rajouter les images

--- lib/story.dart
+++ lib/story.dart
@@ -1,16 +1,17 @@
 class Story {
   int storyNumber;
+  String storyImage;
   String storyText;
   String choix1;
   String choix2;
-  Story(this.storyNumber, this.storyText, this.choix1, this.choix2);
+  Story(this.storyNumber, this.storyImage, this.storyText, this.choix1, this.choix2);
 }
 
 final List<Story> histoire = [
-  Story(0, "texte histoire", "choix 1", "choix 2",),
-  Story(1, "texte histoire", "choix 1", "choix 2",),
-  Story(2, "texte histoire", "choix 1", "choix 2",),
-  Story(3, "texte histoire", "choix 1", "choix 2",),
-  Story(4, "texte histoire", "choix 1", "choix 2",),
-  Story(5, "texte histoire", "choix 1", "choix 2",),
+  Story(0, "image-00.jpg", "texte histoire", "choix 1", "choix 2",),
+  Story(1, "image-00.jpg", "texte histoire", "choix 1", "choix 2",),
+  Story(2, "image-00.jpg", "texte histoire", "choix 1", "choix 2",),
+  Story(3, "image-00.jpg", "texte histoire", "choix 1", "choix 2",),
+  Story(4, "image-00.jpg", "texte histoire", "choix 1", "choix 2",),
+  Story(5, "image-00.jpg", "texte histoire", "choix 1", "choix 2",),
 ];
