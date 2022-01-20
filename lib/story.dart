56622a311f9ab68c4d9127a3f4631c17613dad91
Ryll974 <jullien.patrick@gmail.com>
2022-01-20 11:47:42 +0400

    commit 2 -- définition de la classe Story dans story.dart

---
+++ lib/story.dart
@@ -0,0 +1,16 @@
+class Story {
+  int storyNumber;
+  String storyText;
+  String choix1;
+  String choix2;
+  Story(this.storyNumber, this.storyText, this.choix1, this.choix2);
+}
+
+final List<Story> histoire = [
+  Story(0, "texte histoire", "choix 1", "choix 2",),
+  Story(1, "texte histoire", "choix 1", "choix 2",),
+  Story(2, "texte histoire", "choix 1", "choix 2",),
+  Story(3, "texte histoire", "choix 1", "choix 2",),
+  Story(4, "texte histoire", "choix 1", "choix 2",),
+  Story(5, "texte histoire", "choix 1", "choix 2",),
+];
