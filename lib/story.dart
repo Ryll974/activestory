91fd1177570240da3fe2f22b1568e7b8124b7e09
Ryll974 <jullien.patrick@gmail.com>
2022-01-20 14:01:05 +0400

    commit 6 -- M.A.J de la liste de la classe Story pour intégrer les pointeurs corrects vers les histoires

--- lib/story.dart
+++ lib/story.dart
@@ -10,11 +10,11 @@ class Story {
 }
 
 final List<Story> histoire = [
-  Story(0, "story-00.jpg", "texte histoire 0", "Story 0 choix 1",0, "Story 0 choix 2",0,),
-  Story(1, "story-01.jpg", "texte histoire 1", "Story 1 choix 1",0, "Story 1 choix 2",0,),
-  Story(2, "story-02.jpg", "texte histoire 2", "Story 2 choix 1",0, "Story 2 choix 2",0,),
-  Story(3, "story-03.jpg", "texte histoire 3", "Story 3 choix 1",0, "Story 3 choix 2",0,),
-  Story(4, "story-04.jpg", "texte histoire 4", "Story 4 choix 1",0, "Story 4 choix 2",0,),
-  Story(5, "story-05.jpg", "texte histoire 5", "Story 5 choix 1",0, "Story 5 choix 2",0,),
+  Story(0, "story-00.jpg", "texte histoire 0", "Story 0 choix 1",2, "Story 0 choix 2",1,),
+  Story(1, "story-01.jpg", "texte histoire 1", "Story 1 choix 1",2, "Story 1 choix 2",3,),
+  Story(2, "story-02.jpg", "texte histoire 2", "Story 2 choix 1",5, "Story 2 choix 2",5,),
+  Story(3, "story-03.jpg", "texte histoire 3", "Story 3 choix 1",999, "Story 3 choix 2",999,),
+  Story(4, "story-04.jpg", "texte histoire 4", "Story 4 choix 1",999, "Story 4 choix 2",999,),
+  Story(5, "story-05.jpg", "texte histoire 5", "Story 5 choix 1",999, "Story 5 choix 2",999,),
 ];
 
