4599fecd272c0c594ab748fa89140b557a88df8e
Ryll974 <jullien.patrick@gmail.com>
2022-01-20 13:54:35 +0400

    commit 5 -- ajout des pointeurs dans la classe Story pour diriger les choix vers la suite de l'histoire

--- lib/story.dart
+++ lib/story.dart
@@ -3,16 +3,18 @@ class Story {
   String storyImage;
   String storyText;
   String choix1;
+  int choix1pointer;
   String choix2;
-  Story(this.storyNumber, this.storyImage, this.storyText, this.choix1, this.choix2);
+  int choix2pointer;
+  Story(this.storyNumber, this.storyImage, this.storyText, this.choix1, this.choix1pointer, this.choix2, this.choix2pointer);
 }
 
 final List<Story> histoire = [
-  Story(0, "story-00.jpg", "texte histoire 0", "Story 0 choix 1", "Story 0 choix 2",),
-  Story(1, "story-01.jpg", "texte histoire 1", "Story 1 choix 1", "Story 1 choix 2",),
-  Story(2, "story-02.jpg", "texte histoire 2", "Story 2 choix 1", "Story 2 choix 2",),
-  Story(3, "story-03.jpg", "texte histoire 3", "Story 3 choix 1", "Story 3 choix 2",),
-  Story(4, "story-04.jpg", "texte histoire 4", "Story 4 choix 1", "Story 4 choix 2",),
-  Story(5, "story-05.jpg", "texte histoire 5", "Story 5 choix 1", "Story 5 choix 2",),
+  Story(0, "story-00.jpg", "texte histoire 0", "Story 0 choix 1",0, "Story 0 choix 2",0,),
+  Story(1, "story-01.jpg", "texte histoire 1", "Story 1 choix 1",0, "Story 1 choix 2",0,),
+  Story(2, "story-02.jpg", "texte histoire 2", "Story 2 choix 1",0, "Story 2 choix 2",0,),
+  Story(3, "story-03.jpg", "texte histoire 3", "Story 3 choix 1",0, "Story 3 choix 2",0,),
+  Story(4, "story-04.jpg", "texte histoire 4", "Story 4 choix 1",0, "Story 4 choix 2",0,),
+  Story(5, "story-05.jpg", "texte histoire 5", "Story 5 choix 1",0, "Story 5 choix 2",0,),
 ];
 
