3c768b5bfc7ee7dd0249c88b97848aa2fd7e0aca
Ryll974 <jullien.patrick@gmail.com>
2022-01-20 14:29:08 +0400

    commit 8 -- une version qui fonctionne mais non optimisée

--- lib/story.dart
+++ lib/story.dart
@@ -13,9 +13,9 @@ final List<Story> histoire = [
   Story(0, "images/image-0.jpg", "texte histoire 0", "Story 0 choix 1",2, "Story 0 choix 2",1,),
   Story(1, "images/image-1.jpg", "texte histoire 1", "Story 1 choix 1",2, "Story 1 choix 2",3,),
   Story(2, "images/image-2.jpg", "texte histoire 2", "Story 2 choix 1",5, "Story 2 choix 2",4,),
-  Story(3, "images/image-3.jpg", "texte histoire 3", "Story 3 choix 1",999, "Story 3 choix 2",999,),
-  Story(4, "images/image-4.jpg", "texte histoire 4", "Story 4 choix 1",999, "Story 4 choix 2",999,),
-  Story(5, "images/image-5.jpg", "texte histoire 5", "Story 5 choix 1",999, "Story 5 choix 2",999,),
-  Story(999, "images/image-999.jpg", "terminé", "...",999, "...",999,),
+  Story(3, "images/image-3.jpg", "texte histoire 3", "Story 3 choix 1",6, "Story 3 choix 2",6,),
+  Story(4, "images/image-4.jpg", "texte histoire 4", "Story 4 choix 1",6, "Story 4 choix 2",6,),
+  Story(5, "images/image-5.jpg", "texte histoire 5", "Story 5 choix 1",6, "Story 5 choix 2",6,),
+  Story(6, "images/image-999.jpg", "terminé", "...",6, "...",6,),
 ];
 
